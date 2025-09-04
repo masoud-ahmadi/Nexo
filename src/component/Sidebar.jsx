96fe2f9c011022adaf7c010e2f69d665ee01c4ba
Masoud-Ahmadi <masoudahmadi441@gmail.com>
2025-09-04 21:14:08 +0330

    تغییرات ظاهری

--- src/component/Sidebar.jsx
+++ src/component/Sidebar.jsx
@@ -1,69 +1,41 @@
-import { Link, Links } from "react-router-dom"
-import NexoIcon from "./NexoIcon"
+import { Link, Links } from "react-router-dom";
+import NexoIcon from "./NexoIcon";
 
-const Sidebar = () =>{
-    return <>
+const Sidebar = () => {
+  return (
+    <>
+      <aside className="sidebar h-dvh bg-theme-nexo-content rounded-2xl  max-w-56 w-full overflow-y-hidden  space-y-3">
+        <div className="flex items-center justify-center p-2 border-b">
+          <a href="#" className="max-w-32 block  ">
+            <img src="./Images/Logo/Logo1.png" alt="Logo" />
+          </a>
+        </div>
 
-<aside
-  className="sidebar h-dvh bg-theme-nexo-content rounded-2xl  max-w-56 w-full overflow-y-hidden  space-y-3"
->
-  <div  className="flex items-center justify-center p-2 border-b"
-  >
-    <a href="#" className="max-w-32 block  ">
-      
-        <img  src="./Images/Logo/Logo1.png" alt="Logo" />
-    </a>
-  </div>
+        <div className=" overflow-y-auto no-scrollbar px-3">
+          <nav>
+            <ul>
+              <li className="space-y-2">
+                <div className="bg-gray-200 cursor-pointer hover:bg-theme-nexo-secondary hover:text-white rounded-md flex justify-between items-center p-2">
+                  <div className="flex items-center gap-2">
+                    <NexoIcon name="MdShoppingCart" />
+                    مدیریت فروشگاه
+                  </div>
+                  <NexoIcon name="BiChevronDown" />
+                </div>
+                <ul className="px-2">
+                  <li>
+                    <Link to={"#"} className="menu-item group">
+                      <h3 className="">افزودن مرکز</h3>
+                    </Link>
+                  </li>
+                </ul>
+              </li>
+            </ul>
+          </nav>
+        </div>
+      </aside>
+    </>
+  );
+};
 
-  <div
-    className=" overflow-y-auto no-scrollbar px-3"
-  >
-    <nav>
-      <ul>
-        <li className="space-y-2">
-
-                
-
-          <div className="bg-gray-200 rounded-md flex justify-between items-center p-2">
-            <div
-              className="flex items-center gap-2"
-            >
-            <NexoIcon name="MdShoppingCart"/>
-
-              مدیریت فروشگاه
-            </div>
-          <NexoIcon name="BiChevronDown"/>
-          </div>
-          <ul className="px-2">
-          <li>
-            <Link to={"#"}
-              
-              className="menu-item group"
-            >
-              
-              <h3
-                className=""
-              >
-                افزودن مرکز
-              </h3>
-
-            </Link>
-          </li>
-
-          
-          
-          </ul>
-        </li>
-      </ul>
-      
-    </nav>
-
-    
-    
-  </div>
-</aside>
-</>
-
-}
-
-export default Sidebar
+export default Sidebar;
