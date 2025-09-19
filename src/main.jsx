6d2b1c98ba9d03320f7563a13bad179deea84155
Masoud-Ahmadi <masoudahmadi441@gmail.com>
2025-09-19 21:26:57 +0330

    حذف ایکون

--- src/main.jsx
+++ src/main.jsx
@@ -1,19 +1,13 @@
 import { createRoot } from 'react-dom/client'
 import "./style/App.css"
-import Sidebar from './component/Sidebar'
-import Content from './component/Content'
-import { BrowserRouter } from 'react-router-dom'
-createRoot(document.getElementById('root')).render(<>
-    <BrowserRouter>
-       <div className=' nexo_app bg-theme-nexo-primary'>
-        <div className='flex gap-3'>
-            <Sidebar/>
-        <Content/>
-        </div>
-        
 
-    </div>
-    </BrowserRouter>
+createRoot(document.getElementById('root')).render(<>
+       <div className=' nexo_app bg-theme-nexo-primary'>
+            <div className='flex gap-3'>
+                <Sidebar/>
+                <Content/>
+            </div>
+        </div>
     </>
   
 )
